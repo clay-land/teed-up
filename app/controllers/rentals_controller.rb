b6e6cba648b59e3ee70513f729e1b9c317010082
Chris Boyle <cthboyle6@gmail.com>
2021-05-18 16:54:24 -0400

    created Rentals#show action and temporary view

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -1,2 +1,26 @@
 class RentalsController < ApplicationController
+  before_action :set_rental, only: [:show, :edit, :update, :destroy]
+
+  def show; end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def set_rental
+    @rental = Rental.find_by(params[:id])
+  end
 end
