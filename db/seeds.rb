2839051e9e5b7b9f259a979249e21d80097a5f37
Clayton Landry <clayton.landry@gmail.com>
2021-05-17 20:24:20 -0400

    Seeds created for bag, waiting for direction on proper user creation

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+brands = %w[titleist callaway nike ping taylormade cobra mizuno ]
+
+10.times do
+  User.create(email: Faker::Internet.unique.safe_email)
+  Bag.create(name: "#{brand.sample} #{Faker::Creature::Horse.name}" , description: Faker::Hipster.paragraph(sentence_count: 2), location: Faker::Address.full_address, rate: rand(10.0..50.0).round(2), user_id:  )
+end
