f61b2edd7383ddbbc76fc2dd2ca60c2a6518d8cf
Chris Boyle <cthboyle6@gmail.com>
2021-05-19 13:53:27 -0400

    added validation on end date to rental model

--- app/models/rental.rb
+++ app/models/rental.rb
@@ -4,10 +4,17 @@ class Rental < ApplicationRecord
 
   validates :start_date, :end_date, presence: true
   validate :start_date_cannot_be_in_the_past
+  validate :end_date_cannot_be_before_start_date
 
   def start_date_cannot_be_in_the_past
     if start_date.present? && start_date < Date.today
       errors.add(:start_date, "can't be in the past")
+    end
+  end
+
+  def end_date_cannot_be_before_start_date
+    if end_date.present? && end_date < start_date
+      errors.add(:end_date, "can't be before start date")
     end
   end
 end
