e8a5af7800bb9224a5db7fa69beb93974cea658b
Chris Boyle <cthboyle6@gmail.com>
2021-05-19 13:56:46 -0400

    refactored and tested

--- app/models/rental.rb
+++ app/models/rental.rb
@@ -7,14 +7,10 @@ class Rental < ApplicationRecord
   validate :end_date_cannot_be_before_start_date
 
   def start_date_cannot_be_in_the_past
-    if start_date.present? && start_date < Date.today
-      errors.add(:start_date, "can't be in the past")
-    end
+    errors.add(:start_date, "cannot be in the past") if start_date.present? && start_date < Date.today
   end
 
   def end_date_cannot_be_before_start_date
-    if end_date.present? && end_date < start_date
-      errors.add(:end_date, "can't be before start date")
-    end
+    errors.add(:end_date, "cannot be before start date") if end_date.present? && end_date < start_date
   end
 end
