b7fc761348351110571171852a89f4f921abe1fe
baptiste auffeuvre <baptiste.auffeuvre@gmail.com>
2021-05-20 16:24:20 -0400

    Added JS to display price and number of days

--- app/javascript/plugins/init_flatpickr.js
+++ app/javascript/plugins/init_flatpickr.js
@@ -4,17 +4,41 @@ const flatPickCalendar = () => {
   // First we define two variables that are going to grab our inputs field. You can check the ids of the inputs with the Chrome inspector.
   const startDateInput = document.getElementById('rental_start_date');
   const endDateInput = document.getElementById('rental_end_date');
-// Check that the query selector id matches the one you put around your form.
+
+  const getEvents = () => {
+    const displayRate = document.getElementById("bag-rate");
+    const startDateValue = startDateInput.value;
+    const startDate = new Date(startDateValue);
+    const endDateValue = endDateInput.value;
+    const endDate = new Date(endDateValue);
+    const nbOfDays = (endDate - startDate) / (1000 * 60 * 60 * 24)
+    const list = document.getElementById('display-list');
+
+    list.innerHTML = "";
+    list.insertAdjacentHTML("beforeend", `<div class="d-flex justify-content-between mt-2">\
+                                              <li class="text-total">Total days:</li>\
+                                              <li class="text-total">${nbOfDays} days</li>\
+                                            </div>\
+                                            <div class="d-flex justify-content-between">\
+                                              <li class="text-total">Bag rate:</li>\
+                                              <li class="text-total"> ${displayRate.innerHTML}$/day </li>\
+                                            </div>\
+                                            <hr>\
+                                            <li class="total-price" style="text-align: right;"><strong>Total: ${Math.floor(nbOfDays * displayRate.innerHTML)}$ </strong></li>`);
+  }
+  // Check that the query selector id matches the one you put around your form.
   if (startDateInput) {
     const unavailableDates = JSON.parse(document.querySelector('#bag-rental-dates').dataset.unavailable)
     endDateInput.disabled = true
-
+    
     flatpickr(startDateInput, {
       minDate: "today",
       disable: unavailableDates,
+      altInput: true,
+      altFormat: "F j, Y",
       dateFormat: "Y-m-d",
     });
-
+    
     console.log('im in the file')
     
     startDateInput.addEventListener("change", (e) => {
@@ -24,9 +48,16 @@ const flatPickCalendar = () => {
       flatpickr(endDateInput, {
         minDate: e.target.value,
         disable: unavailableDates,
-        dateFormat: "Y-m-d"
+        dateFormat: "Y-m-d",
+        altInput: true,
+        altFormat: "F j, Y",
       });
-    })
+      // getEvents();
+    });
+    // To display the number of days + total price
+    endDateInput.addEventListener("change", (e) => {
+      getEvents();
+    });
   };
 };
 
