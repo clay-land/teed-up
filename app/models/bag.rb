652c7df5db0b155a524c97e61f66bd39fcf90144
Clayton Landry <clayton.landry@gmail.com>
2021-05-20 11:40:25 -0400

    scope search working from bag model

--- app/models/bag.rb
+++ app/models/bag.rb
@@ -7,4 +7,15 @@ class Bag < ApplicationRecord
 
   validates :name, :description, :location, :rate, presence: true
   validates :rate, numericality: { greater_than_or_equal_to: 0 }
+  include PgSearch::Model
+  pg_search_scope :search_by_loc,
+    against: :location,
+    using: {
+      tsearch: { prefix: true } # <-- now `superman batm` will return something!
+    }
+  pg_search_scope :search_by_brand,
+    against: { name: "A", description: "B" },
+    using: {
+      tsearch: { prefix: true } # <-- now `superman batm` will return something!
+    }
 end
