8e8c1546173b3beebbb76520bc597a8f3049da3c
Chris Boyle <cthboyle6@gmail.com>
2021-05-20 14:41:16 -0400

    added popup markers to map

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -2,32 +2,40 @@ import mapboxgl from 'mapbox-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
 import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
 
+const fitMapToMarkers = (map, markers) => {
+  const bounds = new mapboxgl.LngLatBounds();
+  markers.forEach(marker => bounds.extend([marker.lng, marker.lat]));
+  map.fitBounds(bounds, { padding: 70, maxZoom: 9, duration: 800 });
+};
+
+const addMarkersToMap = (map, markers) => {
+  markers.forEach((marker) => {
+    const popup = new mapboxgl.Popup().setHTML(marker.info_window);
+
+    new mapboxgl.Marker()
+      .setLngLat([marker.lng, marker.lat])
+      .setPopup(popup)
+      .addTo(map);
+      // fitMapToMarkers(map, markers);
+  });
+};
+
 const initMapbox = () => {
   const mapElement = document.getElementById('map');
 
-  const fitMapToMarkers = (map, markers) => {
-    const bounds = new mapboxgl.LngLatBounds();
-    markers.forEach(marker => bounds.extend([marker.lng, marker.lat]));
-    map.fitBounds(bounds, { padding: 70, maxZoom: 9, duration: 800 });
-  };
 
   if (mapElement) { // only build a map if there's a div#map to inject into
     mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
+    const markers = JSON.parse(mapElement.dataset.markers);
+
     const map = new mapboxgl.Map({
       container: 'map',
       style: 'mapbox://styles/mapbox/streets-v10'
     });
 
-    const addMarkersToMap = (map, markers) => {
-      markers.forEach((marker) => {
-        const popup = new mapboxgl.Popup().setHTML(marker.info_window);
+    addMarkersToMap(map, markers);
 
-        new mapboxgl.Marker()
-          .setLngLat([marker.lng, marker.lat]);
-          .setPopup(popup);
-          .addTo(map);
-          fitMapToMarkers(map, markers);
-    });
+    fitMapToMarkers(map, markers);
 
     map.addControl(new MapboxGeocoder({
       accessToken: mapboxgl.accessToken,
