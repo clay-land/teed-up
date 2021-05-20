b1cbe5a403bde16c90d2757469972c67e9c8e03e
Chris Boyle <cthboyle6@gmail.com>
2021-05-20 14:10:05 -0400

    added popup markers to mapbox js function

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -17,13 +17,16 @@ const initMapbox = () => {
       container: 'map',
       style: 'mapbox://styles/mapbox/streets-v10'
     });
-    
-    const markers = JSON.parse(mapElement.dataset.markers);
-    markers.forEach((marker) => {
-      new mapboxgl.Marker()
-        .setLngLat([marker.lng, marker.lat])
-        .addTo(map);
-        fitMapToMarkers(map, markers);
+
+    const addMarkersToMap = (map, markers) => {
+      markers.forEach((marker) => {
+        const popup = new mapboxgl.Popup().setHTML(marker.info_window);
+
+        new mapboxgl.Marker()
+          .setLngLat([marker.lng, marker.lat]);
+          .setPopup(popup);
+          .addTo(map);
+          fitMapToMarkers(map, markers);
     });
 
     map.addControl(new MapboxGeocoder({
