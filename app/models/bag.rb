ba5805065247fb8b379ad6661d8124b701fa2525
baptiste auffeuvre <baptiste.auffeuvre@gmail.com>
2021-05-20 13:22:45 -0400

    Added flat picker + unvailable dates

--- app/models/bag.rb
+++ app/models/bag.rb
@@ -7,4 +7,10 @@ class Bag < ApplicationRecord
 
   validates :name, :description, :location, :rate, presence: true
   validates :rate, numericality: { greater_than_or_equal_to: 0 }
+
+  def unavailable_dates
+    rentals.pluck(:start_date, :end_date).map do |range|
+      { from: range[0], to: range[1] }
+    end
+  end
 end
